c5a7ad24acb20d0d92cb352d2564566a717be376
Julia Eliza <juliaelizaps@gmail.com>
2025-04-22 17:19:53 -0300

    feat(api): create /api/scrape route and handle keyword query param

--- bun-server/index.ts
+++ bun-server/index.ts
@@ -5,12 +5,15 @@ import type { Request, Response } from "express";
 const app = express();
 const port = 8080;
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "hello, express bun here" });
+// GET route at /api/scrape (Ex.request:GET http://localhost:8080/api/scrape?keyword=dress)
+app.get("/api/scrape", (req: Request, res: Response) => {
+    const keyword = req.query.keyword as string;
+    res.json({ message: `Received keyword: ${keyword}` });
 });
 
+// Starting the server
 app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
+    console.log(`Listening on port ${port}...`);
 });
 
 
