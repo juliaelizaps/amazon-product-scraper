931243d7c46f4b2e3868af286ea7ea79d96944be
Julia Eliza <juliaelizaps@gmail.com>
2025-04-22 16:25:33 -0300

    feat(server): setup initial express server with Bun

--- bun-server/index.ts
+++ bun-server/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+// @ts-ignore
+import express from "express";
+import type { Request, Response } from "express";
+
+const app = express();
+const port = 8080;
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "hello, express bun here" });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
+
+
+
+
+
